b959a46accd4d3ad805f033448259ed34bc905bd
Riya-negi02 <negiriya2005@gmail.com>
2026-02-07 12:18:11 +0530

    changes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553  // to declare constant
+let accountEmail= 'riya@google.com' 
+var accountPassword= '12345'
+accountCity= 'jaipur' // can use but not recommended
+let accountState;
+/*
+prefer not ot use var because of issue in block scope and functional scope
+*/
+//const keyword can't be changed
+accountEmail= 'hc@hc.com'
+accountPassword= '2121'
+accountCity= 'Bengluru'
+
+console.log(accountId)
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
