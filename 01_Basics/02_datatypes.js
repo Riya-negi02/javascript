fc5ce232a0dcf2be3228b99c47643855b9841ba0
Riya-negi02 <negiriya2005@gmail.com>
2026-02-08 10:29:34 +0530

    chanes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+//alert("hello") // we are using nodejs, not browser
+
+let name= 'Riya'
+let age= 22
+let idLoggedIn= false
+
+// premitive DT
+//number => 2^53
+//bigint 
+//string => " "
+//boolean => true/false
+//null = standalone value
+// undefined => value not defined
+// symbol => unique
+
+//object
+
+console.log(typeof "Riya"); 
+console.log( typeof null); //type h iska object
