02ab71740d86d076ce6972d97d7c076f9009cc92
Riya-negi02 <negiriya2005@gmail.com>
2026-02-22 13:05:27 +0530

    commit changes

--- 01_Basics/03_conversion.js
+++ 01_Basics/03_conversion.js
@@ -16,3 +16,14 @@ console.log(booleanisLoggedIn);
 let someNumber= 33
 let stringNumber= String(someNumber)
 console.log(stringNumber);
+
+//operations
+let value =3
+let negvalue = -value
+console.log(negvalue); // will give negative of the value
+
+//string concatenation is allowed
+//concatenation number and string is allowed
+
+console.log("1"+ 2 + 2) //it's giving 122
+console.log(1 + 2 + "2") // it's giving 32
