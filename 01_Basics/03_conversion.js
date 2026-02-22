cf34cf668bd03693d9065379d8c263b65dddd16f
Riya-negi02 <negiriya2005@gmail.com>
2026-02-22 11:25:24 +0530

    commit changes

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score=33
+console.log(typeof score);
+// const {score} = req.body
+let valueInNumber = Number(score)  //string to number conversion
+console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc"=> NaN
+//true=>1
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+let someNumber= 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
