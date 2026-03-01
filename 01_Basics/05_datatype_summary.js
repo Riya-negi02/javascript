a3aebb2889fd95246d0636f2667c4d62b547f79d
Riya-negi02 <negiriya2005@gmail.com>
2026-03-01 12:32:45 +0530

    modified

---
+++ 01_Basics/05_datatype_summary.js
@@ -0,0 +1,6 @@
+/* priimitive datatype: 7
+string, number, boolean, null, undefined, symbol, bigInt */
+
+/* Reference type (Non-primitve): Array, objects, functions */
+
+//typeof(variable): gives data of that variable
