4e84a5fb6bee12fe9fae5f2fc508bf1e16cf2e14
Riya-negi02 <negiriya2005@gmail.com>
2026-03-01 11:51:23 +0530

    modified

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0); //false
+console.log(nul == 0);  //false
+console.log(numll >= 0);  //true
+
+//reason: comparison convert null to a number, treation it as 0.  
+
+//=== strict check: check both values and datatypes
+
+console.log("2"==2); //true
+console.log("2"=== 2); //false
