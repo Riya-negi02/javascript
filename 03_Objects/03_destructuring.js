7ec7a1cf1b7eb03f6961dde7d1e881bbe275fba3
Riya-negi02 <negiriya2005@gmail.com>
2026-03-05 01:19:50 +0530

    changes

---
+++ 03_Objects/03_destructuring.js
@@ -0,0 +1,12 @@
+const course= {
+    coursename: 'js in hindi',
+    prince: '999',
+    instructor: "rashmi"
+}
+
+const {instructor: inst} =course  //gives the value and we cahnged the name as well
+
+//json structure
+{
+    //info , keys and values are string
+}
