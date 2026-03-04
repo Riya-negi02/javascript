291d13aedf0408b5cf3527ff984c93f520edc768
Riya-negi02 <negiriya2005@gmail.com>
2026-03-04 23:49:37 +0530

    changes

--- 02_Arrays/02_array2.js
+++ 02_Arrays/02_array2.js
@@ -8,4 +8,16 @@ const allheros=marvel_heros.concat(dc)
 //console.log(allheros); // merged array
 
 //spread 
-const all_new_heros = [...marvel_heros, ...dc]
+const all_new_heros = [...marvel_heros, ...dc]    // element got spread(individual)
+
+const anotherarray= [1,2,3,[4,5,6],7,[6,7[4,5]]]
+const real_another_array= anotherarray.flat(Infinity)  // will make array inside array in flat version
+
+Array.isArray('riya') //gives true false
+Array.from('riya')  // covert into an array
+
+let score1= 100
+let score2= 300
+let score3= 600
+
+console.log(Array.of(score1, score2, score3)); //make array of these individual values
