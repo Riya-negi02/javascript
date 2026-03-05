02d5240dc36118ddfa2783dad33a129de05beef1
Riya-negi02 <negiriya2005@gmail.com>
2026-03-05 18:59:19 +0530

    ifelse

---
+++ 05_controlflow/01_inro.js
@@ -0,0 +1,30 @@
+//if
+if(condition){
+    //code
+}else if(condition){
+    //code
+} else{
+    //code
+}
+
+//switch
+switch (key){
+    case value:
+    break;
+
+    default:
+        break;
+
+}
+
+//falsy values
+// false, 0, -0, BigInt 0n, " ", null, undefined, NaN
+
+//null coalescing operator
+let val1;
+val1= 5 ?? 10    //5
+
+
+//terniary operator
+
+condition ? true : false 
